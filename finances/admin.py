d76d192308f6db4cd8bceaaa3242d773d7b8cc8d
Jairo Milagre da Fonseca Jr <jrmilagre@gmail.com>
2024-11-02 13:00:45 -0300

    Inclusão do campo accounts > opening_balance

--- finances/admin.py
+++ finances/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Account
 
-admin.site.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['account', 'opening_balance']
+
+admin.site.register(Account, AccountAdmin)
