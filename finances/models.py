5e513f10a8662bf74060a7eb1259e3de6aed0e9c
Jairo Milagre da Fonseca Jr <jrmilagre@gmail.com>
2024-11-02 10:14:08 -0300

    Segundo commit

--- finances/models.py
+++ finances/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# My models.
 class Account(models.Model):
     account =models.CharField(max_length=60)
+    opening_balance = models.DecimalField(max_digits=15, decimal_places=2, default=0.00)
 
     def __str__(self):
         return self.account
