c113fd4576c72b08bdd9f50a5f6d6ff8635eb8c3
Jairo Milagre da Fonseca Jr <jrmilagre@gmail.com>
2024-11-17 06:05:57 -0300

    Add opening_date field to Account model and apply migrations

--- finances/admin.py
+++ finances/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Account
 
+
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ['account', 'opening_balance']
+    list_display = ['account', 'opening_balance', 'opening_date']
+
 
 admin.site.register(Account, AccountAdmin)
--- finances/models.py
+++ finances/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 # My models.
+
+
 class Account(models.Model):
-    account =models.CharField(max_length=60)
+
+    account = models.CharField(max_length=60)
     opening_balance = models.DecimalField(max_digits=15, decimal_places=2, default=0.00)
+    opening_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.account
